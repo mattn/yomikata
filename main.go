0547361d7ca44df8fc264ad17fe5350f55e94fbc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-07-23 15:24:03 +0900

    first import

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "yomikawa [word]")
+		os.Exit(1)
+	}
+	word := os.Args[1]
+	resp, err := http.Get("http://yomikata.org/word/" + word)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, os.Args[0]+":", err)
+		os.Exit(1)
+	}
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, os.Args[0]+":", err)
+		os.Exit(1)
+	}
+	if doc.Find("#word").Text() == "" {
+		fmt.Fprintln(os.Stderr, "わかりません")
+		os.Exit(1)
+	}
+	doc.Find(".spAns").Each(func(_ int, s *goquery.Selection) {
+		fmt.Printf("%s (%s)\n", s.Find(".psAns").Text(), s.Find(".psPt").Text())
+	})
+}
